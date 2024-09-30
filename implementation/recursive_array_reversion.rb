4d3c7a1c66047f1e1987de3daaa583ea4cd58a69
Temitayo Adeleke <lekedevrepo@gmail.com>
2024-09-30 13:39:59 +0300

    Update recursive array reversion implementation

--- implementation/recursive_array_reversion.rb
+++ implementation/recursive_array_reversion.rb
@@ -2,7 +2,7 @@
 def reverse_array(array)
   return [] if array.nil? || array.empty?
 
-  return reverse_array(array[1..]) << array[0]
+  reverse_array(array[1..]) << array[0]
 end
 
 p reverse_array([1, 2, 3, 4, 5])
