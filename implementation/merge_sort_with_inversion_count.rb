bd65709b574f8460e73cb0863c6ed2d48aaf70bc
Temitayo Adeleke <lekedevrepo@gmail.com>
2024-10-07 08:10:13 +0300

    Reimplement merge sort with inversion count

--- implementation/merge_sort_with_inversion_count.rb
+++ implementation/merge_sort_with_inversion_count.rb
@@ -1,31 +1,33 @@
 
-def merge_sort_inversion_count(array)
+def merge_sort_with_inversion_count(array)
   return [array, 0] if array.size <= 1
-  
+
   mid = array.size / 2
-  left, left_count = merge_sort_inversion_count(array[0...mid])
-  right, right_count = merge_sort_inversion_count(array[mid..])
+  left, left_inversion_count = merge_sort_with_inversion_count(array[0...mid])
+  right, right_inversion_count = merge_sort_with_inversion_count(array[mid..])
 
-  inversion_count, merge_sort_array = merge_with_inversion(left, right)
-
-  [merge_sort_array, left_count + right_count + inversion_count]
+  merge, inversion_count = merge_with_inversion_count(left, right)
+  [merge, inversion_count + left_inversion_count + right_inversion_count]
 end
 
-def merge_with_inversion(left, right)
+def merge_with_inversion_count(left, right)
   sorted_array = []
-  counter = 0
-  while !left.empty? && !right.empty?
+  inversion_count = 0
+
+  until left.empty? || right.empty?
     if left[0] <= right[0]
       sorted_array << left.shift
     else
-      counter += left.size
       sorted_array << right.shift
+      inversion_count += left.size
     end
   end
-  [counter, sorted_array + left + right]
+
+  sorted_array.concat(left).concat(right)
+  [sorted_array, inversion_count]
 end
 
-arr = [2, 4, 1, 3, 5]
-sorted_array, inversion_count = merge_sort_inversion_count(arr)
-p sorted_array
+arr = [3, 5, 1, 9, 10, 2, 3, 6, 8, 7]
+sorted_arr, inversion_count = merge_sort_with_inversion_count(arr)
+p sorted_arr
 p inversion_count
