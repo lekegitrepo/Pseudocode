125d8c8c956f705f0da7f23873c7d0a03d543fd4
Temitayo Adeleke <lekedevrepo@gmail.com>
2024-10-29 01:13:55 +0200

    Rewrite tail recursive sum

--- implementation/tail_recursive_sum.rb
+++ implementation/tail_recursive_sum.rb
@@ -2,7 +2,7 @@
 def tail_recursive_sum(array, acc = 0)
   return acc if array.nil? || array.empty?
 
-  return tail_recursive_sum(array[1..], acc + array[0])
+  tail_recursive_sum(array[1..], acc + array[0])
 end
 
 p tail_recursive_sum([3, 9])
