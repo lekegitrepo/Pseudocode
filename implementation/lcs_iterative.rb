250e341304ae58e8ea5aca9d0fa9e131ad834f4f
Temitayo Adeleke <lekedevrepo@gmail.com>
2024-11-07 08:47:38 +0200

    Re-implement lcs with iterative

--- implementation/lcs_iterative.rb
+++ implementation/lcs_iterative.rb
@@ -4,17 +4,16 @@ def lcs(str1, str2)
 
   (1..str1.size).each do |i|
     (1..str2.size).each do |j|
-      if str1[i - 1] == str2[j - 1]
-        dp[i][j] = dp[i - 1][j - 1] + 1
+      if str1[i-1] == str2[j-1]
+        dp[i][j] = dp[i-1][j-1] + 1
       else
-        dp[i][j] = [dp[i - 1][j], dp[i][j - 1]].max
+        dp[i][j] = [dp[i-1][j], dp[i][j-1]].max
       end
     end
   end
-  
   dp[str1.size][str2.size]
 end
 
 str_two = 'ABCDAB'
-str_one = 'ABCDAB'
+str_one = 'ABD'
 puts lcs(str_one, str_two)
