58e3a9b2782585bb9644c74b30bf40d4ca1f18d9
Temitayo Adeleke <lekedevrepo@gmail.com>
2024-11-15 10:31:54 +0200

    Add an example

--- implementation/binary_tree.rb
+++ implementation/binary_tree.rb
@@ -25,3 +25,14 @@ class BinaryTree
     result
   end
 end
+
+tree = BinaryTree.new(8)
+
+tree.root.left = Node.new(3)
+tree.root.right = Node.new(15)
+tree.root.left.left = Node.new(2)
+tree.root.left.right = Node.new(5)
+tree.root.right.left = Node.new(10)
+tree.root.right.right = Node.new(20)
+
+p tree.inorder_traversal(tree.root)
