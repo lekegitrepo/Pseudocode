9bac3decb942a806aeda01a0b3ae6ef74c8793d2
Temitayo Adeleke <lekedevrepo@gmail.com>
2024-11-20 18:29:28 +0200

    Reimplement tail recursion

--- implementation/tail_recursive_sum.rb
+++ implementation/tail_recursive_sum.rb
@@ -2,7 +2,7 @@
 def tail_recursive_sum(array, acc = 0)
   return acc if array.nil? || array.empty?
 
-  tail_recursive_sum(array[1..], acc + array[0])
+  tail_recursive_sum(array[1..], array[0] + acc)
 end
 
 p tail_recursive_sum([3, 9])
