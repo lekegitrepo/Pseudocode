79488131cf0374cd5f37541866f1d9a1d5977969
Temitayo Adeleke <lekedevrepo@gmail.com>
2024-11-28 07:10:07 +0200

    Reimplement merge sort with inversion count

--- implementation/merge_sort_with_inversion_count.rb
+++ implementation/merge_sort_with_inversion_count.rb
@@ -1,13 +1,14 @@
 
 def merge_sort_with_inversion_count(array)
-  return [array, 0] if array.size <= 1
+  return [array, 0] unless array.size > 1
 
   mid = array.size / 2
+
   left, left_inversion_count = merge_sort_with_inversion_count(array[0...mid])
   right, right_inversion_count = merge_sort_with_inversion_count(array[mid..])
 
-  merge, inversion_count = merge_with_inversion_count(left, right)
-  [merge, inversion_count + left_inversion_count + right_inversion_count]
+  sorted_array, inversion_count = merge_with_inversion_count(left, right)
+  [sorted_array, left_inversion_count + right_inversion_count + inversion_count]
 end
 
 def merge_with_inversion_count(left, right)
@@ -15,7 +16,7 @@ def merge_with_inversion_count(left, right)
   inversion_count = 0
 
   until left.empty? || right.empty?
-    if left[0] <= right[0]
+    if left[0] < right[0]
       sorted_array << left.shift
     else
       sorted_array << right.shift
@@ -23,8 +24,7 @@ def merge_with_inversion_count(left, right)
     end
   end
 
-  sorted_array.concat(left).concat(right)
-  [sorted_array, inversion_count]
+  [sorted_array.concat(left).concat(right), inversion_count]
 end
 
 arr = [3, 5, 1, 9, 10, 2, 3, 6, 8, 7]
