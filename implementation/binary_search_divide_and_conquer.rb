211ce0a8692005a0b67a7ac769eb799c9ead6780
Temitayo Adeleke <lekedevrepo@gmail.com>
2024-12-14 20:01:25 +0200

    Reimplement binary_search

--- implementation/binary_search_divide_and_conquer.rb
+++ implementation/binary_search_divide_and_conquer.rb
@@ -1,11 +1,25 @@
 
+# def binary_search(array, target, left, right)
+#   return -1 if left > right
+
+#   mid = (left + right) / 2
+
+#   if array[mid] == target
+#     return mid
+#   elsif array[mid] > target
+#     return binary_search(array, target, left, mid - 1)
+#   else
+#     return binary_search(array, target, mid + 1, right)
+#   end
+# end
+
 def binary_search(array, target, left, right)
   return -1 if left > right
 
   mid = (left + right) / 2
 
   if array[mid] == target
-    return mid
+    return array[mid]
   elsif array[mid] > target
     return binary_search(array, target, left, mid - 1)
   else
