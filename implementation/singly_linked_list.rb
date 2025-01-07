e184a596e84790a1948a6824d5da1db80d85a5d3
Temitayo Adeleke <lekedevrepo@gmail.com>
2025-01-07 14:11:13 +0200

    Implement insert method

--- implementation/singly_linked_list.rb
+++ implementation/singly_linked_list.rb
@@ -27,6 +27,25 @@ class LinkedList
     end
   end
 
+  def insert(value)
+    return 'The value already exist in nodes' if find(value)
+
+    new_node = Node.new(value)
+
+    if @head.nil? || value < @head.data
+      new_node.next = @head
+      @head = new_node
+      return
+    end
+
+    current_node = @head
+    current_node = current_node.next while current_node.next && current_node.next.data < value
+
+    new_node.next = current_node.next
+
+    current_node.next = new_node
+  end
+
   def find(value)
     return @head if @head.data == value || @head.nil?
 
@@ -41,8 +60,11 @@ linked_list = LinkedList.new
 
 linked_list.append(10)
 linked_list.append(28)
+linked_list.append(30)
 
-# p linked_list
+linked_list.insert(12)
 
-p linked_list.find(20)
+p linked_list
+
+p linked_list.find(28)
 p linked_list.find(10)
