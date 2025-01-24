669af4e2bfae1dec6e0f5d5067c36581297654f4
Temitayo Adeleke <lekedevrepo@gmail.com>
2025-01-24 12:27:23 +0200

    Add test for append method

--- spec/doubly_linked_list_spec.rb
+++ spec/doubly_linked_list_spec.rb
@@ -17,7 +17,15 @@ RSpec.describe 'Doubly-LinkedList' do
     let(:doubly_linked_list) { DoublyLinkedList.new }
 
     context 'when the first node is appended to the list' do
+      before { doubly_linked_list.append(4) }
       it 'set the head and the tail to the only node' do
+        expect(doubly_linked_list.head.data).to eq 4
+        expect(doubly_linked_list.tail.data).to eq 4
+      end
+    end
+
+    context 'when the node is prepend to the list' do
+      it 'set the node as the tail' do
       end
     end
   end
