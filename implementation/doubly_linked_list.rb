abdfe2e8d91c828abd8e2555df6e5ad1e29b3172
Temitayo Adeleke <lekedevrepo@gmail.com>
2025-02-04 12:02:29 +0200

    Update delete method

--- implementation/doubly_linked_list.rb
+++ implementation/doubly_linked_list.rb
@@ -78,24 +78,26 @@ class DoublyLinkedList
       @head = @head.next
     else
       current_node = @head
-      current_node = current_node.next while current_node && current_node.next.data != value
+      current_node = current_node.next while current_node && current_node.data != value
 
       if current_node.next
-        current_node.next.next.prev = current_node
-        current_node.next = current_node.next.next
+        current_node.prev.next = current_node.next
+        current_node.next.prev = current_node.prev
       end
+
+      @tail = current_node.next if current_node.next.next.nil?
     end
   end
 end
 
-doubly_linked_list = DoublyLinkedList.new
+# doubly_linked_list = DoublyLinkedList.new
 
-doubly_linked_list.append(8)
-doubly_linked_list.append(13)
-doubly_linked_list.append(18)
+# doubly_linked_list.append(8)
+# doubly_linked_list.append(13)
+# doubly_linked_list.append(18)
 
-p doubly_linked_list
+# p doubly_linked_list
 
-doubly_linked_list.delete(13)
+# doubly_linked_list.delete(13)
 
-p doubly_linked_list
+# p doubly_linked_list
