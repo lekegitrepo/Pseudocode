bbb63612b7195c04d5b75fbe80235dea6f7bd9e6
Temitayo Adeleke <lekedevrepo@gmail.com>
2025-02-15 12:47:58 +0200

    Create binary_search_tree_spec.rb

---
+++ spec/binary_search_tree_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../implementation/binary_search_tree.rb'
+
+RSpec.describe 'Binary Search Tree' do
+  let(:bst) { BST.new(20) }
+
+  context 'when new node is inserted' do
+    it 'inserts the node' do
+    end
+  end
+
+  describe 'Searching for a node' do
+    context 'when a node is found' do
+      it 'returns the node' do
+      end
+    end
+  end
+
+  describe 'Traversing the binary tree' do
+    context 'when a node is found' do
+      it 'returns the values in sorted order' do
+      end
+    end
+  end
+end
+
