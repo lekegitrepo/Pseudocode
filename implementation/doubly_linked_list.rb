7c233c3fb781f2e3d48c4c067a6a19f2b4eccd41
Temitayo Adeleke <lekedevrepo@gmail.com>
2025-02-19 09:01:56 +0200

    Update doubly_linked_list

--- implementation/doubly_linked_list.rb
+++ implementation/doubly_linked_list.rb
@@ -45,9 +45,10 @@ class DoublyLinkedList
   def insert(value)
     node = Node.new(value)
 
-    if @head.nil? || @head.data > value
-      @tail = node if @head.nil?
-
+    if @head.nil?
+      @head = node
+      @tail = node 
+    elsif @head.data > value
       node.next = @head
       @head.prev = node
       @head = node
@@ -76,26 +77,46 @@ class DoublyLinkedList
 
     if @head.data == value
       @head = @head.next
+    elsif @tail.data == value
+      @tail = @tail.prev
     else
-      current_node = @head
+      current_node = @head.next
       current_node = current_node.next while current_node && current_node.data != value
 
       if current_node.next
         current_node.prev.next = current_node.next
         current_node.next.prev = current_node.prev
       end
-
-      @tail = current_node.next if current_node.next.next.nil?
     end
+    @tail = nil if @head.nil?
+  end
+
+  def display_forward
+    current_node = @head
+    while current_node
+      print "#{current_node.data} -> "
+      current_node = current_node.next
+    end
+    puts "\n----"
+  end
+
+  def display_reverse
+    current_node = @tail
+    while current_node
+      print "#{current_node.data} -> "
+      current_node = current_node.prev
+    end
+    puts "\n----"
   end
 end
 
-# doubly_linked_list = DoublyLinkedList.new
+doubly_linked_list = DoublyLinkedList.new
 
-# doubly_linked_list.append(8)
-# doubly_linked_list.append(13)
-# doubly_linked_list.append(18)
-
+doubly_linked_list.append(8)
+doubly_linked_list.append(13)
+doubly_linked_list.append(18)
+doubly_linked_list.display_forward
+doubly_linked_list.display_reverse
 # p doubly_linked_list
 
 # doubly_linked_list.delete(13)
