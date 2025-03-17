629af5a7f29032c38640c9de0eb7aae0482d21ae
Temitayo Adeleke <lekedevrepo@gmail.com>
2025-03-17 09:47:03 +0200

    Outline specs

--- spec/avl_tree_spec.rb
+++ spec/avl_tree_spec.rb
@@ -1,8 +1,24 @@
 require_relative '../implementation/avl_tree.rb'
 
-RSpec.describe '' do
-  context '' do
-    it '' do
+RSpec.describe 'AVL Tree' do
+  describe '#insert' do
+    context 'when tree is already balanced' do
+      it 'insert to the appropriate subtree' do
+      end
+    end
+  end
+
+  describe '#search' do
+    context 'when tree is already balanced' do
+      it 'returns the value' do
+      end
+    end
+  end
+
+  describe '#delete' do
+    context 'when tree is already balanced' do
+      it 'deletes the node with specified data' do
+      end
     end
   end
 end
