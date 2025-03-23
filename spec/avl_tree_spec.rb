2065125a2365e4cce0f4e21dff78b77dc6af06ef
Temitayo Adeleke <lekedevrepo@gmail.com>
2025-03-23 19:34:20 +0200

    Add basic insert specs

--- spec/avl_tree_spec.rb
+++ spec/avl_tree_spec.rb
@@ -1,9 +1,19 @@
 require_relative '../implementation/avl_tree.rb'
 
 RSpec.describe 'AVL Tree' do
+  let(:avl_tree) { AVLTree.new }
+
   describe '#insert' do
+    before { [20, 18, 24].each {avl_tree.insert(_1) } }
+
     context 'when tree is already balanced' do
-      it 'insert to the appropriate subtree' do
+      before { avl_tree.insert(29)}
+
+      it 'insert to the right subtree' do
+        expect(@root.data).to eq 20
+        expect(@root.left.data).to eq 18
+        expect(@root.right.data).to eq 24
+        expect(@root.right.right.data).to eq 29
       end
     end
   end
