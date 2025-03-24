dd8d6cfde2ea5aa28f14a4c5b645ad00c9fb94e7
Temitayo Adeleke <lekedevrepo@gmail.com>
2025-03-24 05:30:50 +0200

    Add left rotation context specs

--- spec/avl_tree_spec.rb
+++ spec/avl_tree_spec.rb
@@ -4,17 +4,36 @@ RSpec.describe 'AVL Tree' do
   let(:avl_tree) { AVLTree.new }
 
   describe '#insert' do
-    before { [20, 18, 24].each {avl_tree.insert(_1) } }
-
     context 'when tree is already balanced' do
-      before { avl_tree.insert(29)}
+      before { [20, 18, 24].each { avl_tree.insert(_1) } }
 
-      it 'insert to the right subtree' do
+      it 'inserts to the right subtree' do
+        avl_tree.insert(29)
         expect(@root.data).to eq 20
         expect(@root.left.data).to eq 18
         expect(@root.right.data).to eq 24
         expect(@root.right.right.data).to eq 29
       end
+    end
+
+    context 'when the tree needs left rotation' do
+      before { [8, 15, 17].each { avl_tree.insert(_1) } }
+
+      it 'changes the root node, balance the tree and insert the node' do
+        expect(@root.data).to eq 8
+        avl_tree.insert(28)
+        # expect { @root }.to change(@root.data).from(8).to(15)
+        expect(@root.data).to eq 15
+      end
+    end
+
+    context 'when the tree needs right rotation' do
+    end
+
+    context 'when the tree needs left-right rotation' do
+    end
+
+    context 'when the tree needs right-left rotation' do
     end
   end
 
