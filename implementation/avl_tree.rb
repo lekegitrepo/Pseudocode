2766ff54d0ad5fc1832b9cc9a05be612c29d4200
Temitayo Adeleke <lekedevrepo@gmail.com>
2025-03-27 21:45:34 +0200

    Implement logic for insertion

--- implementation/avl_tree.rb
+++ implementation/avl_tree.rb
@@ -7,6 +7,7 @@ class AVLTree
   end
 
   def insert(value)
+    @root = insert_node(@root, value)
   end
 
   def delete
@@ -16,6 +17,53 @@ class AVLTree
   end
 
   def traverse
+  end
+
+  private
+
+  def insert_node(node, value)
+    return Node.new(value) if node.nil?
+
+    if value > node.data
+      node.right = insert_node(node.right, value)
+    else
+      node.left = insert_node(node.left, value)
+    end
+
+    node.height = 1 + [height(node.left), height(node.right)].max
+
+    tree_balance = height(node.left) - height(node.right)
+
+    return right_rotate(node) if tree_balance > 1 && value < node.left.data
+
+    return left_rotate(node) if tree_balance < -1 && value > node.right.data
+
+    # left - right rotation
+    if tree_balance > 1 && value > node.left.data
+      node.left = left_rotate(node.left)
+      return right_rotate(node)
+    end
+
+    # right - left rotation
+    if tree_balance < -1 && value < node.right.data
+      node.right = right_rotate(node.right)
+      return left_rotate(node)
+    end
+
+    node
+  end
+
+  def height(node)
+    return 0 if node.nil?
+
+    return node.height
+  end
+
+  def right_rotate(node)
+  end
+
+  def left_rotate(node)
+    node
   end
 end
 
@@ -28,6 +76,6 @@ class Node
     @data = data
     @left = nil
     @right = nil
-    @height = nil
+    @height = 0
   end
 end
