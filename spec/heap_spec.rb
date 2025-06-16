fc69316d47f4b53286d44167947dbd0f44e556e1
Temitayo Adeleke <lekedevrepo@gmail.com>
2025-06-16 07:51:41 +0300

    Update test block

--- spec/heap_spec.rb
+++ spec/heap_spec.rb
@@ -2,7 +2,7 @@
 
 require_relative '../implementation/heap'
 
-RSpec.describe 'Heap' do
+RSpec.describe Heap do
   subject(:heap) { described_class.new }
 
   describe 'insertion' do
@@ -11,7 +11,9 @@ RSpec.describe 'Heap' do
 
       let(:arr) { [3, 5, 9, 6, 8, 10, 12] }
 
-      expect(arr[0]).to eq 2
+      it 'returns an array in min-heap structure' do
+        expect(arr[0]).to eq 2
+      end
     end
   end
 end
