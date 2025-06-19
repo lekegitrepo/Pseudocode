585fd0ff801bebf4e7955632c921b76004d8246d
Temitayo Adeleke <lekedevrepo@gmail.com>
2025-06-19 05:39:30 +0300

    Implement test for max heap

--- spec/heap_spec.rb
+++ spec/heap_spec.rb
@@ -5,7 +5,7 @@ require_relative '../implementation/heap'
 RSpec.describe Heap do
   subject(:heap) { described_class.new }
 
-  describe 'insertion' do
+  describe 'min insertion' do
     context 'when min value is inserted' do
       before { heap.min_heap_insertion(arr, 2) }
 
@@ -13,6 +13,18 @@ RSpec.describe Heap do
 
       it 'returns an array in min-heap structure' do
         expect(arr[0]).to eq 2
+      end
+    end
+  end
+
+  describe 'max insertion' do
+    context 'when max value is inserted' do
+      before { heap.max_heap_insertion(arr, 20) }
+
+      let(:arr) { [3, 5, 9, 6, 8, 10, 12] }
+
+      it 'returns an array in max-heap structure' do
+        expect(arr[0]).to eq 20
       end
     end
   end
