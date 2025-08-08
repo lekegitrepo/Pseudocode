d374eb23b0f9856a2146691a8dce970298f0e44f
Temitayo Adeleke <lekedevrepo@gmail.com>
2025-08-08 06:21:36 +0300

    Implement bfs_traversal

--- implementation/graph.rb
+++ implementation/graph.rb
@@ -1,3 +1,4 @@
+require 'set'
 
 def add_edge(graph, node, neighbor, directed = false)
   graph = {} if graph.nil?
@@ -15,4 +16,26 @@ def add_edge(graph, node, neighbor, directed = false)
   graph[node] << neighbor if !graph[node].include?(neighbor) && !directed
 
   graph
+end
+
+def bfs_traversal(graph, node)
+  queue = []
+  visited = Set.new
+  queue << node
+  visited << node
+
+  while !queue.empty?
+    current_node = queue.shift
+
+    # process current_node here
+
+    graph[current_node].each do |neighbor|
+      unless visited.include?(neighbor)
+        queue << neighbor
+        visited << neighbor
+      end
+    end
+  end
+
+  visited
 end
